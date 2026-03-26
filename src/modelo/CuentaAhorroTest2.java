7ac414631c53ed796ddc56c6598294fe9dead1e7
kennethdavidlopezgsanjosemlg-ux <kennethdavilopezg.sanjosemlg@fundacionloyola.net>
2026-03-26 18:58:01 +0100

    commit test exception realizadas

---
+++ src/modelo/CuentaAhorroTest2.java
@@ -0,0 +1,35 @@
+package modelo;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CuentaAhorroTest2 {
+	private CuentaAhorro cuentaAhorro;
+
+	@Test
+	public void testIngresar() {
+		cuentaAhorro = new CuentaAhorro("Test", 123, 0.05);
+		Exception exception = assertThrows( IllegalArgumentException.class, () -> cuentaAhorro.ingresar(-1234) );
+		
+		assertEquals("La cantidad a ingresar no puede ser menor a 0",exception.getMessage());
+	}
+	
+	@Test
+	public void testReintegrar() {
+		cuentaAhorro = new CuentaAhorro("Test", 123, 0.05);
+		Exception exception = assertThrows( IllegalArgumentException.class, () -> cuentaAhorro.reintegrar(-1234) );
+		
+		assertEquals("La cantidad a ingresar no puede ser menor a 0",exception.getMessage());
+	}
+	
+	@Test
+	public void testReintegrar2() {
+		cuentaAhorro = new CuentaAhorro("Test", 123, 0.05);
+		Exception exception = assertThrows( IllegalArgumentException.class, () -> cuentaAhorro.reintegrar(1234) );
+		
+		assertEquals("No hay suficiente saldo para realizar la operación",exception.getMessage());
+	}
+
+}
--- src/modelo/CuentaBancaria.java
+++ src/modelo/CuentaBancaria.java
@@ -24,11 +24,22 @@ public abstract class CuentaBancaria {
     }
 
     public void ingresar(double cantidad) {
-
+    	if (cantidad < 0) {
+    		throw new IllegalArgumentException("La cantidad a ingresar no puede ser menor a 0");
+    	}
+    	
+    	
         saldo += cantidad;
     }
 
     public void reintegrar(double cantidad) {
+    	if (cantidad < 0) {
+    		throw new IllegalArgumentException("La cantidad a ingresar no puede ser menor a 0");
+    	}
+    	
+    	  if (cantidad > saldo) {
+              throw new IllegalArgumentException("No hay suficiente saldo para realizar la operación");
+          }
 
         saldo -= cantidad;
     }
